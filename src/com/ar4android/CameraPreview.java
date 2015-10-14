3460e6db8fe5a6581fb086e33c2625092382bfac
ethan_yu <ethan_yu@htc.com>
2015-10-14 14:27:46 +0800

    no message

--- src/com/ar4android/CameraPreview.java
+++ src/com/ar4android/CameraPreview.java
@@ -1,11 +1,10 @@
-/* CameraPreview - CameraAccessAndroid Example
+/* CameraPreview - SuperimposeJME Example
  * 
- * Example Chapter 2
+ * Example Chapter 3
  * accompanying the book
  * "Augmented Reality for Android Application Development", Packt Publishing, 2013.
  * 
  * Copyright � 2013 Jens Grubert, Raphael Grasset / Packt Publishing.
- * 
  */
 
 package com.ar4android;
@@ -20,10 +19,12 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
     private static final String TAG = "CameraPreview";
 	private SurfaceHolder mHolder;
     private Camera mCamera;
+    private Camera.PreviewCallback mCameraPreviewCallback;
 
-    public CameraPreview(Context context, Camera camera) {
+    public CameraPreview(Context context, Camera camera, Camera.PreviewCallback cameraCallback) {
         super(context);
         mCamera = camera;
+        mCameraPreviewCallback=cameraCallback;
         // Install a SurfaceHolder.Callback so we get notified when the
         // underlying surface is created and destroyed.
         mHolder = getHolder();
@@ -41,10 +42,15 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
     public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
         // If your preview can change or rotate, take care of those events here.
         // Make sure to stop the preview before resizing or reformatting it.
+
         if (mHolder.getSurface() == null){
           // preview surface does not exist
+        	Log.e(TAG,"no preview surface (surfaceChanged)");
           return;
         }
+       
+        
+        
         // stop preview before making changes
         try {
             mCamera.stopPreview();
@@ -56,8 +62,16 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
         // reformatting changes here
         // start preview with new settings
         try {
-            mCamera.setPreviewDisplay(mHolder);
-            mCamera.startPreview();
+        	if (mCamera ==null)
+        	{
+        		Log.e(TAG,"no camera in start preview");
+        	}
+        	else
+        	{
+        		mCamera.setPreviewCallback(mCameraPreviewCallback);
+        		mCamera.setPreviewDisplay(mHolder);
+        		mCamera.startPreview();
+        	}
         } catch (Exception e){
             Log.d(TAG, "Error starting camera preview: " + e.getMessage());
         }
